9738dcdf462b2f252812bd3ae3c2127b8cfc8a41
cdnicoll <cdnicoll@gmail.com>
2019-04-17 13:11:43 -0700

    Added circular references between company and user

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 [Video Series](https://www.udemy.com/graphql-with-react-course)
 
 [Source](https://github.com/StephenGrider/GraphQLCast)
+
+[local graphQL test bed](http://localhost:4000/graphql)
--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,13 +1,37 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
 const axios = require('axios')
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        .then(resp => resp.data)
+      },
+    },
+  }),
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLInt },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: { 
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(resp => resp.data)
+      }
+    },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -18,6 +42,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLInt } },
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(resp => resp.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
         .then(resp => resp.data);
       },
     },
